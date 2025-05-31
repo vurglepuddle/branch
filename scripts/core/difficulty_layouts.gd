e89da945b0d951c155b001551073637af07a2864
Gabriel Burdich <gabriel.burdich@gmail.com>
2025-05-31 23:07:28 +0300

    Expert difficulty added

--- scripts/core/difficulty_layouts.gd
+++ scripts/core/difficulty_layouts.gd
@@ -210,15 +210,129 @@ const PREVIEW_LAYOUTS = {
 		{"tile_type_enum": BranchType.STRAIGHT, "pos": Vector2i(5, 9), "rot_idx": 0, "state": "dead"},
 		{"tile_type_enum": BranchType.STRAIGHT, "pos": Vector2i(5, 8), "rot_idx": 0, "state": "dead"},
 		{"tile_type_enum": BranchType.TERMINAL, "pos": Vector2i(5, 7), "rot_idx": 2, "state": "dead"},
-		
-		
-
 
 	],
 	"expert": [
 
-		{"tile_type_enum": BranchType.TERMINAL, "pos": Vector2i(0, 1), "rot_idx": 1}, # Opening upwards
+#embellishment 1
+		{"tile_type_enum": BranchType.TERMINAL, "pos": Vector2i(5, 0), "rot_idx": 3, "state": "dead"},
+		{"tile_type_enum": BranchType.STRAIGHT, "pos": Vector2i(4, 0), "rot_idx": 1, "state": "dead"},
+		{"tile_type_enum": BranchType.STRAIGHT, "pos": Vector2i(3, 0), "rot_idx": 1, "state": "dead"},
+		{"tile_type_enum": BranchType.STRAIGHT, "pos": Vector2i(2, 0), "rot_idx": 1, "state": "dead"},
+		{"tile_type_enum": BranchType.STRAIGHT, "pos": Vector2i(1, 0), "rot_idx": 1, "state": "dead"},
+		{"tile_type_enum": BranchType.BEND, "pos": Vector2i(0, 0), "rot_idx": 1, "state": "dead"},
 		
+		{"tile_type_enum": BranchType.BEND, "pos": Vector2i(0, 1), "rot_idx": 0, "state": "dead"},
+		{"tile_type_enum": BranchType.STRAIGHT, "pos": Vector2i(4, 1), "rot_idx": 1, "state": "dead"},
+		{"tile_type_enum": BranchType.STRAIGHT, "pos": Vector2i(3, 1), "rot_idx": 1, "state": "dead"},
+		{"tile_type_enum": BranchType.STRAIGHT, "pos": Vector2i(2, 1), "rot_idx": 1, "state": "dead"},
+		{"tile_type_enum": BranchType.STRAIGHT, "pos": Vector2i(1, 1), "rot_idx": 1, "state": "dead"},
+		{"tile_type_enum": BranchType.BEND, "pos": Vector2i(5, 1), "rot_idx": 2, "state": "dead"},
+		
+		{"tile_type_enum": BranchType.STRAIGHT, "pos": Vector2i(5, 2), "rot_idx": 0, "state": "dead"},
+		{"tile_type_enum": BranchType.THREE, "pos": Vector2i(5, 3), "rot_idx": 2, "state": "dead"},
+		{"tile_type_enum": BranchType.TERMINAL, "pos": Vector2i(4, 3), "rot_idx": 1, "state": "dead"},
+		{"tile_type_enum": BranchType.STRAIGHT, "pos": Vector2i(5, 4), "rot_idx": 0, "state": "dead"},
+		{"tile_type_enum": BranchType.STRAIGHT, "pos": Vector2i(5, 5), "rot_idx": 0, "state": "dead"},
+		{"tile_type_enum": BranchType.STRAIGHT, "pos": Vector2i(5, 6), "rot_idx": 0, "state": "dead"},
+		{"tile_type_enum": BranchType.STRAIGHT, "pos": Vector2i(5, 7), "rot_idx": 0, "state": "dead"},
+		{"tile_type_enum": BranchType.STRAIGHT, "pos": Vector2i(5, 8), "rot_idx": 0, "state": "dead"},
+		{"tile_type_enum": BranchType.STRAIGHT, "pos": Vector2i(5, 9), "rot_idx": 0, "state": "dead"},
+		
+		{"tile_type_enum": BranchType.THREE, "pos": Vector2i(5, 10), "rot_idx": 2, "state": "dead"},
+		{"tile_type_enum": BranchType.THREE, "pos": Vector2i(4, 10), "rot_idx": 3, "state": "dead"},
+		{"tile_type_enum": BranchType.TERMINAL, "pos": Vector2i(3, 10), "rot_idx": 1, "state": "dead"},
+		{"tile_type_enum": BranchType.STRAIGHT, "pos": Vector2i(4, 9), "rot_idx": 0, "state": "dead"},
+		{"tile_type_enum": BranchType.TERMINAL, "pos": Vector2i(4, 8), "rot_idx": 2, "state": "dead"},
+		
+		{"tile_type_enum": BranchType.STRAIGHT, "pos": Vector2i(5, 11), "rot_idx": 0, "state": "dead"},
+		{"tile_type_enum": BranchType.STRAIGHT, "pos": Vector2i(5, 12), "rot_idx": 0, "state": "dead"},
+		{"tile_type_enum": BranchType.STRAIGHT, "pos": Vector2i(5, 13), "rot_idx": 0, "state": "dead"},
+		{"tile_type_enum": BranchType.STRAIGHT, "pos": Vector2i(5, 14), "rot_idx": 0, "state": "dead"},
+		{"tile_type_enum": BranchType.BEND, "pos": Vector2i(5, 15), "rot_idx": 3, "state": "dead"},
+
+		{"tile_type_enum": BranchType.THREE, "pos": Vector2i(4, 15), "rot_idx": 3, "state": "dead"},
+		{"tile_type_enum": BranchType.THREE, "pos": Vector2i(3, 15), "rot_idx": 3, "state": "dead"},
+		{"tile_type_enum": BranchType.THREE, "pos": Vector2i(2, 15), "rot_idx": 3, "state": "dead"},
+		{"tile_type_enum": BranchType.THREE, "pos": Vector2i(1, 15), "rot_idx": 3, "state": "dead"},
+		{"tile_type_enum": BranchType.BEND, "pos": Vector2i(0, 15), "rot_idx": 0, "state": "dead"},
+		
+		{"tile_type_enum": BranchType.STRAIGHT, "pos": Vector2i(4, 14), "rot_idx": 0, "state": "dead"},
+		{"tile_type_enum": BranchType.STRAIGHT, "pos": Vector2i(4, 13), "rot_idx": 0, "state": "dead"},
+		{"tile_type_enum": BranchType.TERMINAL, "pos": Vector2i(4, 12), "rot_idx": 2, "state": "dead"},
+		
+		{"tile_type_enum": BranchType.TERMINAL, "pos": Vector2i(3, 14), "rot_idx": 2, "state": "dead"},
+		
+		{"tile_type_enum": BranchType.STRAIGHT, "pos": Vector2i(2, 14), "rot_idx": 0, "state": "dead"},
+		{"tile_type_enum": BranchType.STRAIGHT, "pos": Vector2i(2, 13), "rot_idx": 0, "state": "dead"},
+		{"tile_type_enum": BranchType.TERMINAL, "pos": Vector2i(2, 12), "rot_idx": 2, "state": "dead"},
+		
+		{"tile_type_enum": BranchType.STRAIGHT, "pos": Vector2i(1, 14), "rot_idx": 0, "state": "dead"},
+		{"tile_type_enum": BranchType.STRAIGHT, "pos": Vector2i(1, 13), "rot_idx": 0, "state": "dead"},
+		{"tile_type_enum": BranchType.STRAIGHT, "pos": Vector2i(1, 12), "rot_idx": 0, "state": "dead"},
+		{"tile_type_enum": BranchType.TERMINAL, "pos": Vector2i(1, 11), "rot_idx": 2, "state": "dead"},
+		
+		{"tile_type_enum": BranchType.STRAIGHT, "pos": Vector2i(0, 14), "rot_idx": 0, "state": "dead"},
+		{"tile_type_enum": BranchType.STRAIGHT, "pos": Vector2i(0, 13), "rot_idx": 0, "state": "dead"},
+		{"tile_type_enum": BranchType.STRAIGHT, "pos": Vector2i(0, 12), "rot_idx": 0, "state": "dead"},
+		{"tile_type_enum": BranchType.TERMINAL, "pos": Vector2i(0, 11), "rot_idx": 2, "state": "dead"},
+#embellishment 2
+		{"tile_type_enum": BranchType.TERMINAL, "pos": Vector2i(0, 5), "rot_idx": 2, "state": "dead"},
+		{"tile_type_enum": BranchType.STRAIGHT, "pos": Vector2i(0, 6), "rot_idx": 0, "state": "dead"},
+		{"tile_type_enum": BranchType.TERMINAL, "pos": Vector2i(0, 7), "rot_idx": 0, "state": "dead"},
+		
+#Э
+		{"tile_type_enum": BranchType.TERMINAL, "pos": Vector2i(0, 2), "rot_idx": 1, "state": "alive"},
+		{"tile_type_enum": BranchType.BEND, "pos": Vector2i(1, 2), "rot_idx": 2, "state": "alive"},
+		{"tile_type_enum": BranchType.TERMINAL, "pos": Vector2i(0, 3), "rot_idx": 1, "state": "alive"},
+		{"tile_type_enum": BranchType.THREE, "pos": Vector2i(1, 3), "rot_idx": 2, "state": "alive"},
+		{"tile_type_enum": BranchType.TERMINAL, "pos": Vector2i(0, 4), "rot_idx": 1, "state": "alive"},
+		{"tile_type_enum": BranchType.BEND, "pos": Vector2i(1, 4), "rot_idx": 3, "state": "alive"},
+		
+#K
+		{"tile_type_enum": BranchType.TERMINAL, "pos": Vector2i(2, 2), "rot_idx": 2, "state": "alive"},
+		{"tile_type_enum": BranchType.BEND, "pos": Vector2i(3, 2), "rot_idx": 1, "state": "alive"},
+		{"tile_type_enum": BranchType.TERMINAL, "pos": Vector2i(4, 2), "rot_idx": 3, "state": "alive"},
+		{"tile_type_enum": BranchType.THREE, "pos": Vector2i(2, 3), "rot_idx": 0, "state": "alive"},
+		{"tile_type_enum": BranchType.THREE, "pos": Vector2i(3, 3), "rot_idx": 2, "state": "alive"},
+		{"tile_type_enum": BranchType.TERMINAL, "pos": Vector2i(2, 4), "rot_idx": 0, "state": "alive"},
+		{"tile_type_enum": BranchType.BEND, "pos": Vector2i(3, 4), "rot_idx": 0, "state": "alive"},
+		{"tile_type_enum": BranchType.TERMINAL, "pos": Vector2i(4, 4), "rot_idx": 3, "state": "alive"},
+
+#c
+		{"tile_type_enum": BranchType.BEND, "pos": Vector2i(1, 5), "rot_idx": 1, "state": "alive"},
+		{"tile_type_enum": BranchType.TERMINAL, "pos": Vector2i(2, 5), "rot_idx": 3, "state": "alive"},
+		{"tile_type_enum": BranchType.STRAIGHT, "pos": Vector2i(1, 6), "rot_idx": 0, "state": "alive"},
+		{"tile_type_enum": BranchType.BEND, "pos": Vector2i(1, 7), "rot_idx": 0, "state": "alive"},
+		{"tile_type_enum": BranchType.TERMINAL, "pos": Vector2i(2, 7), "rot_idx": 3, "state": "alive"},
+#P
+		{"tile_type_enum": BranchType.BEND, "pos": Vector2i(3, 5), "rot_idx": 1, "state": "alive"},
+		{"tile_type_enum": BranchType.BEND, "pos": Vector2i(4, 5), "rot_idx": 2, "state": "alive"},
+		{"tile_type_enum": BranchType.STRAIGHT, "pos": Vector2i(3, 6), "rot_idx": 0, "state": "alive"},
+		{"tile_type_enum": BranchType.STRAIGHT, "pos": Vector2i(4, 6), "rot_idx": 0, "state": "alive"},
+		{"tile_type_enum": BranchType.TERMINAL, "pos": Vector2i(3, 7), "rot_idx": 0, "state": "alive"},
+		{"tile_type_enum": BranchType.TERMINAL, "pos": Vector2i(4, 7), "rot_idx": 0, "state": "alive"},
+#e
+		{"tile_type_enum": BranchType.BEND, "pos": Vector2i(0, 8), "rot_idx": 1, "state": "alive"},
+		{"tile_type_enum": BranchType.TERMINAL, "pos": Vector2i(1, 8), "rot_idx": 3, "state": "alive"},
+		{"tile_type_enum": BranchType.THREE, "pos": Vector2i(0, 9), "rot_idx": 0, "state": "alive"},
+		{"tile_type_enum": BranchType.TERMINAL, "pos": Vector2i(1, 9), "rot_idx": 3, "state": "alive"},
+		{"tile_type_enum": BranchType.BEND, "pos": Vector2i(0, 10), "rot_idx": 0, "state": "alive"},
+		{"tile_type_enum": BranchType.TERMINAL, "pos": Vector2i(1, 10), "rot_idx": 3, "state": "alive"},
+		
+#R
+		{"tile_type_enum": BranchType.BEND, "pos": Vector2i(2, 8), "rot_idx": 1, "state": "alive"},
+		{"tile_type_enum": BranchType.BEND, "pos": Vector2i(3, 8), "rot_idx": 2, "state": "alive"},
+		{"tile_type_enum": BranchType.THREE, "pos": Vector2i(2, 9), "rot_idx": 0, "state": "alive"},
+		{"tile_type_enum": BranchType.BEND, "pos": Vector2i(3, 9), "rot_idx": 3, "state": "alive"},
+		{"tile_type_enum": BranchType.TERMINAL, "pos": Vector2i(2, 10), "rot_idx": 0, "state": "alive"},
+		
+#t
+		{"tile_type_enum": BranchType.TERMINAL, "pos": Vector2i(2, 11), "rot_idx": 1, "state": "alive"},
+		{"tile_type_enum": BranchType.THREE, "pos": Vector2i(3, 11), "rot_idx": 1, "state": "alive"},
+		{"tile_type_enum": BranchType.TERMINAL, "pos": Vector2i(4, 11), "rot_idx": 3, "state": "alive"},
+		{"tile_type_enum": BranchType.STRAIGHT, "pos": Vector2i(3, 12), "rot_idx": 0, "state": "alive"},
+		{"tile_type_enum": BranchType.TERMINAL, "pos": Vector2i(3, 13), "rot_idx": 0, "state": "alive"},
 	],
 	"torrero": [
  
